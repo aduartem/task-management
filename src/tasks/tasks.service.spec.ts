8928fee8de98e19c70d2e43a90bd38ca6cf6e548
Andres Duarte <andres.duarte.marquez@gmail.com>
2023-11-11 20:35:34 -0300

    unit tests to tasksService.getTasks()

---
+++ src/tasks/tasks.service.spec.ts
@@ -0,0 +1,40 @@
+import { Test } from '@nestjs/testing';
+import { TasksService } from './tasks.service';
+import { TasksRepository } from './tasks.repository';
+
+const mockTasksRepository = () => ({
+  getTasks: jest.fn(),
+});
+
+const mockUser = {
+  id: 'someId',
+  username: 'Ariel',
+  password: 'somePassword',
+  tasks: [],
+};
+
+describe('TasksService', () => {
+  let tasksService: TasksService;
+  let tasksRepository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        TasksService,
+        { provide: TasksRepository, useFactory: mockTasksRepository },
+      ],
+    }).compile();
+
+    tasksService = module.get(TasksService);
+    tasksRepository = module.get(TasksRepository);
+  });
+
+  describe('getTasks', () => {
+    it('calls TasksRepository.getTasks and returns the result', async () => {
+      tasksRepository.getTasks.mockResolvedValue('someValue');
+      const result = await tasksService.getTasks(null, mockUser);
+      expect(tasksRepository.getTasks).toHaveBeenCalled();
+      expect(result).toEqual('someValue');
+    });
+  });
+});
